bf5fdcdff43b229fee3fa64fc4cac7ab02660056
Polina <polina.lipse@gmai.com>
2023-08-17 11:10:37 +0300

    fixed sidebar

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";;
 import { SheetTrigger, Sheet, SheetContent  } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
@@ -26,9 +25,9 @@ const MobileSidebar = ({
   return (
     <Sheet>
     <SheetTrigger>
-      <Button variant="ghost" size="icon" className="md:hidden">
+      <span className="md:hidden">
         <Menu />
-      </Button>
+      </span>
     </SheetTrigger>
     <SheetContent side="left" className="p-0">
       <Sidebar apiLimitCount={apiLimitCount} />
