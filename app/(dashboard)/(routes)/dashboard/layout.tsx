8297b87fe50e0725d802096119edda445bd6698b
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-08-17 20:21:35 +0300

    updated dashboard layout

--- app/(dashboard)/(routes)/dashboard/layout.tsx
+++ app/(dashboard)/(routes)/dashboard/layout.tsx
@@ -9,8 +9,17 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const isPro = await checkSubscription();
 
   return (
+    <div className="h-full relative">
+    <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+      <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+    </div>
+    <main className="md:pl-72 pb-10">
+      <Navbar />
+      {children}
+    </main>
+  </div>
   )
-  
+
   }
 
 export default DashboardLayout;
